a7ec9b4cc6957ffd0bf21c7664722df67230b276
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2010-12-12 15:37:37 +0100

    Added Service Reference to TV4Home

--- MPWebStream/Streamer.cs
+++ MPWebStream/Streamer.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Web;
+using System.ServiceModel;
+using MPWebStream.TVEInteraction;
 
 namespace MPWebStream {
     public class Streamer {
         public static void run(HttpResponse Response) {
-            Response.Write("HEllo!");
+            // connect to TV4Home service
+            // FIXME: make hostname configurable
+            EndpointAddress address = new EndpointAddress(String.Format("http://{0}:4321/TV4Home.Server.CoreService/TVEInteractionService", "mediastreamer.lan"));
+            TVEInteractionClient tvWebClient = new TVEInteractionClient("BasicHttpBinding_ITVEInteraction", address);
         }
     }
 }
