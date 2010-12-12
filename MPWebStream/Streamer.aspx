1d4158599b8b2ceac9ccd8267a591687cca54773
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2010-12-12 15:22:43 +0100

    Created project

---
+++ MPWebStream/Streamer.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Language="C#" %>
+<script runat="server" language="C#">
+    public void Page_Load(object sender, EventArgs e) {
+        MPWebStream.Streamer.run(Response);
+    }
+</script>
---
+++ MPWebStream/Streamer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web;
+
+namespace MPWebStream {
+    public class Streamer {
+        public static void run(HttpResponse Response) {
+            Response.Write("HEllo!");
+        }
+    }
+}
