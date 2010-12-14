8276e8dd6a343d80faec29ec6e0429c3996b516e
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2010-12-14 18:49:00 +0100

    Committed missing file...

---
+++ Cassini-v35/ConsoleMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Cassini {
+    class ConsoleMain {
+        public static int Main(string[] args) {
+            // parse arguments
+            if (args.Length != 2) {
+                System.Console.WriteLine("Usage: Cassini.exe [port] [path]");
+                return 1;
+            }
+            int port = Int32.Parse(args[0]);
+            string physicalPath = args[1];
+
+            // start server
+            Server server = new Server(port, "/", physicalPath);
+            server.Start();
+
+            // loop until forever
+            while (true) {
+                // TODO: maybe something to break out earlier? 
+                Thread.Sleep(60000);
+            }
+
+            // stop server
+            server.Stop();
+
+            return 0;
+        }
+    }
+}
