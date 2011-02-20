952b85b5395959ffdfa62e2bed7ba184e48fd685
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-02-20 18:58:01 +0100

    Create page with a list of all channels and links to their stream

---
+++ MPWebStream/Default.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page EnableViewState="false" Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MPWebStream.Site.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>MPWebStream</title>
+</head>
+<body>
+    <form runat="server">
+        <asp:BulletedList ID="Channels" runat="server" DisplayMode="HyperLink">
+        </asp:BulletedList>
+    </form>
+</body>
+</html>
---
+++ MPWebStream/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace MPWebStream.Site {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            // this is probably not the way you should do it, but I'm not a ASP.NET coder and I don't have time to learn it. Suggestions or patches welcome. 
+            if (!Authentication.authenticate(HttpContext.Current))
+                return;
+
+            Configuration config = new Configuration();
+            MediaStream remoteControl = new MediaStream();
+            foreach (Channel channel in remoteControl.GetChannels()) {
+                Channels.Items.Add(new ListItem(channel.DisplayName, remoteControl.GetTvStreamUrl(channel.IdChannel, config.Username, config.Password)));
+            }
+            Channels.DisplayMode = BulletedListDisplayMode.HyperLink;
+        }
+    }
+}
