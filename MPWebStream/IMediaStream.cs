4b1246742680653cf559f770a86739aef0a04f25
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-03-20 15:45:08 +0100

    Add a JSON output for the webservice

--- MPWebStream/IMediaStream.cs
+++ MPWebStream/IMediaStream.cs
@@ -1,34 +1,51 @@
 ﻿using System;
 using System.ServiceModel;
+using System.Collections.Generic;
+using System.ServiceModel.Web;
 
 namespace MPWebStream.Site {
     [ServiceContract]
-    interface IMediaStream {
+    public interface IMediaStream {
         [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, 
+            UriTemplate = "GetChannel?idChannel={idChannel}")]
         Channel GetChannel(int idChannel);
 
         [OperationContract]
-        System.Collections.Generic.List<Channel> GetChannels();
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        List<Channel> GetChannels();
 
         [OperationContract]
-        System.Collections.Generic.List<Recording> GetRecordings();
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        List<Recording> GetRecordings();
 
         [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, 
+            UriTemplate = "GetRecordingStreamUrl?idRecording={idRecording}&username={username}&password={password}")]
         string GetRecordingStreamUrl(int idRecording, string username, string password);
 
         [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, 
+            UriTemplate = "GetTranscodedRecordingStreamUrl?idRecording={idRecording}&username={username}&password={password}&idTranscoder={idTranscoder}")]
         string GetTranscodedRecordingStreamUrl(int idRecording, string username, string password, int idTranscoder);
 
         [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, 
+            UriTemplate = "GetTranscodedTvStreamUrl?idChannel={idChannel}&username={username}&password={password}&idTranscoder={idTranscoder}")]
         string GetTranscodedTvStreamUrl(int idChannel, string username, string password, int idTranscoder);
 
         [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, 
+            UriTemplate = "GetTranscoderById?id={id}")]
         Transcoder GetTranscoderById(int id);
 
         [OperationContract]
-        System.Collections.Generic.List<Transcoder> GetTranscoders();
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        List<Transcoder> GetTranscoders();
 
         [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, 
+            UriTemplate = "GetTvStreamUrl?idChannel={idChannel}&username={username}&password={password}")]
         string GetTvStreamUrl(int idChannel, string username, string password);
     }
 }
