8d8c0ac7efb3fde52bac48198190715c6ec311f6
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-03-24 18:10:58 +0100

    Set version number to 0.9.9.0 for release

--- MPWebStream/Properties/AssemblyInfo.cs
+++ MPWebStream/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MPWebStream")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © 2010, 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.9.0")]
+[assembly: AssemblyFileVersion("0.9.9.0")]
--- TvServerPlugin/TvServerPlugin.cs
+++ TvServerPlugin/TvServerPlugin.cs
@@ -34,7 +34,7 @@ namespace MPWebStream.TvServerPlugin {
         }
 
         public string Version {
-            get { return "0.1.0.0"; }
+            get { return "0.9.9.0"; }
         }
 
         public string Author {
