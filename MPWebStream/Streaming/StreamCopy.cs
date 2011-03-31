d0b4140af9732ec5515206870454d50094747741
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-03-31 16:32:57 +0200

    Make sure the destinatino stream is writable

--- MPWebStream/Streaming/StreamCopy.cs
+++ MPWebStream/Streaming/StreamCopy.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿#region Copyright
+/* 
+ *  Copyright (C) 2011, Oxan
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+#endregion
+
+using System;
 using System.Threading;
 using System.IO;
 
@@ -14,9 +36,10 @@ namespace MPWebStream.Streaming {
             while (true) {
                 do {
                     read = original.Read(buffer, 0, buffer.Length);
-                } while (read == 0 && original.CanRead);
+                } while (read == 0 && original.CanRead && destination.CanWrite);
 
-                destination.Write(buffer, 0, read);
+                if(destination.CanWrite)
+                    destination.Write(buffer, 0, read);
             }
         }
 
