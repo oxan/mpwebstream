a813a021d03cd0a90622707195c68608c59166c2
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-03-31 16:14:30 +0200

    Add StreamCopy class, which copies a stream to another stream asynchronously

---
+++ MPWebStream/Streaming/StreamCopy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace MPWebStream.Streaming {
+    public class StreamCopy {
+        private const int _defaultBufferSize = 8192;
+
+        delegate void BlockingCopyDelegate(Stream original, Stream destination, int bufferSize);
+
+        private static void BlockingCopy(Stream original, Stream destination, int bufferSize) {
+            byte[] buffer = new byte[bufferSize];
+            int read;
+            while (true) {
+                do {
+                    read = original.Read(buffer, 0, buffer.Length);
+                } while (read == 0 && original.CanRead);
+
+                destination.Write(buffer, 0, read);
+            }
+        }
+
+        private static void DoneCallback(IAsyncResult result) {
+            // do nothing with it
+        }
+
+        public static void AsyncStreamCopy(Stream original, Stream destination) {
+            BlockingCopyDelegate d = new BlockingCopyDelegate(StreamCopy.BlockingCopy);
+            d.BeginInvoke(original, destination, _defaultBufferSize, new AsyncCallback(DoneCallback), new object());
+        }
+    }
+}
