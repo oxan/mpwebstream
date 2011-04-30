81a59dabf1c8540b471281b20af6f4e189a498a3
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2011-04-30 16:23:10 +0200

    Abstract the parsing of parameters for a stream interface to a class

---
+++ MPWebStream/StreamInterface.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MPWebStream.Site.Service;
+
+namespace MPWebStream.Site {
+    public abstract class StreamInterface : System.Web.UI.Page {
+        protected class StreamData {
+            public string name;
+            public string url;
+        }
+
+        protected StreamData parseParameters() {
+            StreamData ret = new StreamData();
+            MediaStream control = new MediaStream();
+            int transcoder = Int32.Parse(HttpContext.Current.Request.Params["transcoder"]);
+            Configuration config = new Configuration();
+
+            // live tv
+            int channelId = 0;
+            Int32.TryParse(HttpContext.Current.Request.Params["channel"], out channelId);
+            if (channelId != 0) {
+                Channel channel = control.GetChannel(channelId);
+                ret.url = control.GetTranscodedTvStreamUrl(channel.IdChannel, config.Username, config.Password, transcoder);
+                ret.name = channel.DisplayName;
+            }
+
+            // recording
+            int recordingId = 0;
+            Int32.TryParse(HttpContext.Current.Request.Params["recording"], out recordingId);
+            if (recordingId != 0) {
+                Recording recording = control.GetRecording(recordingId);
+                ret.url = control.GetTranscodedRecordingStreamUrl(recordingId, config.Username, config.Password, transcoder);
+                ret.name = recording.Title;
+            }
+
+            return ret;
+        }
+    }
+}
--- MPWebStream/VLC.aspx.cs
+++ MPWebStream/VLC.aspx.cs
@@ -20,22 +20,14 @@
  */
 #endregion
 
-using MPWebStream.Site.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace MPWebStream.Site {
-    public partial class VLC : System.Web.UI.Page {
+    public partial class VLC : StreamInterface {
         protected void Page_Load(object sender, EventArgs e) {
-            MediaStream control = new MediaStream();
-            Channel channel = control.GetChannel(Int32.Parse(HttpContext.Current.Request.Params["channel"]));
-            Configuration config = new Configuration();
-            url.Value = control.GetTranscodedTvStreamUrl(channel.IdChannel, config.Username, config.Password, Int32.Parse(HttpContext.Current.Request.Params["transcoder"]));
-            name.Value = channel.DisplayName;
+            StreamData data = parseParameters();
+            url.Value = data.url;
+            name.Value = data.name;
         }
     }
 }
